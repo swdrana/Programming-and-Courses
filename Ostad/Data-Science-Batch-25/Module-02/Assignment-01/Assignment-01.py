745d02fdd7da9f02f5ff4a96b686c112f8918896
swdrana <tctr2s@gmail.com>
2023-04-11 16:15:01 +0600

    unique and duplicate both created and update readme and generate pdf

--- Ostad/Data-Science-Batch-25/Module-02/Assignment-01/Assignment-01.py
+++ Ostad/Data-Science-Batch-25/Module-02/Assignment-01/Assignment-01.py
@@ -1,11 +1,9 @@
 givenList = [1, 5, 6, 5, 1, 2, 3]
-newList = []
+uniqueList = []
 duplicateList = []
 for item in givenList:
-    if item not in newList:
-        newList.append(item)
+    if item not in uniqueList:
+        uniqueList.append(item)
     else:
         duplicateList.append(item)
-
-
 print(duplicateList)
--- Ostad/Data-Science-Batch-25/Module-02/Assignment-01/READme.md
+++ Ostad/Data-Science-Batch-25/Module-02/Assignment-01/READme.md
@@ -5,3 +5,17 @@
 ```python
 [1, 5, 6, 5, 1, 2, 3]
 ```
+
+## Solution:
+
+```python
+givenList = [1, 5, 6, 5, 1, 2, 3]
+uniqueList = []
+duplicateList = []
+for item in givenList:
+    if item not in uniqueList:
+        uniqueList.append(item)
+    else:
+        duplicateList.append(item)
+print(duplicateList)
+```
