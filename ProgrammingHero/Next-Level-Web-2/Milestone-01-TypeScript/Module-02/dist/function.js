d43bef922f9fbd18bb8cdcf88a47b9659e4c2dc5
swdrana <tctr2s@gmail.com>
2023-05-01 18:25:05 +0600

    2.6 Function Done

---
+++ ProgrammingHero/Next-Level-Web-2/Milestone-01-TypeScript/Module-02/dist/function.js
@@ -0,0 +1,9 @@
+"use strict";
+// Normal Function
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+add(2, 5);
+// Arrow Function
+const welcome = (fName, lName) => `Welcome ${fName} ${lName}`;
+console.log(welcome("Masud", "Rana"));
