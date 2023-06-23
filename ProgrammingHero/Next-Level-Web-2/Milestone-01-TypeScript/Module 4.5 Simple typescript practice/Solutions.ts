d1da25d84bb0a7e2d6b8f15091023b6e15b5e572
swdrana <tctr2s@gmail.com>
2023-06-23 11:47:28 +0600

    prob 2 solved

--- ProgrammingHero/Next-Level-Web-2/Milestone-01-TypeScript/Module 4.5 Simple typescript practice/Solutions.ts
+++ ProgrammingHero/Next-Level-Web-2/Milestone-01-TypeScript/Module 4.5 Simple typescript practice/Solutions.ts
@@ -10,3 +10,23 @@ const userInfo: [number, string, string, boolean, undefined, string] = [
 console.log(userInfo)
 
 // Problem NO 2
+/**
+ * Write a TypeScript function that takes in two arrays of numbers as parameters. The function should compare the elements in both arrays and return a new array that contains only the elements that are present in both arrays.
+
+For example, if the first array is [1, 2, 3, 4, 5] and the second array is [2, 4, 6, 8], the function should return a new array with the elements 2 and 4 because they are present in both arrays.
+
+The function should handle arrays of any length and should return the resulting array in the same order as they appear in the first array.
+ */
+type NumberArrayType = number[]
+function findSmiler(arr1:NumberArrayType, arr2:NumberArrayType):NumberArrayType{
+    const newArray:NumberArrayType=[]
+    for(const num of arr1){
+        if (arr2.includes(num)){
+            newArray.push(num)
+        }
+    }
+    return newArray;
+}
+const fArray:NumberArrayType = [1, 2, 3, 4, 5];
+const sArray:NumberArrayType = [2, 4, 6, 8];
+console.log(findSmiler(fArray, sArray))
