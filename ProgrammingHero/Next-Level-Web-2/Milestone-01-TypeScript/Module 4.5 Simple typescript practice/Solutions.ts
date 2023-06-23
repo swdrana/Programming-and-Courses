14216169de556b225fd8ca427504d904eaddd5e5
swdrana <tctr2s@gmail.com>
2023-06-23 20:13:47 +0600

    prob 3 solved but confused when i call the fuction i dont give <> but still its working

--- ProgrammingHero/Next-Level-Web-2/Milestone-01-TypeScript/Module 4.5 Simple typescript practice/Solutions.ts
+++ ProgrammingHero/Next-Level-Web-2/Milestone-01-TypeScript/Module 4.5 Simple typescript practice/Solutions.ts
@@ -1,4 +1,4 @@
-// Problem NO 1
+// ===================== Problem NO 1 ===================== //
 const userInfo: [number, string, string, boolean, undefined, string] = [
   101,
   "Ema",
@@ -7,26 +7,59 @@ const userInfo: [number, string, string, boolean, undefined, string] = [
   ,
   "2023",
 ];
-console.log(userInfo)
+// console.log(userInfo);
 
-// Problem NO 2
-/**
- * Write a TypeScript function that takes in two arrays of numbers as parameters. The function should compare the elements in both arrays and return a new array that contains only the elements that are present in both arrays.
-
-For example, if the first array is [1, 2, 3, 4, 5] and the second array is [2, 4, 6, 8], the function should return a new array with the elements 2 and 4 because they are present in both arrays.
-
-The function should handle arrays of any length and should return the resulting array in the same order as they appear in the first array.
- */
-type NumberArrayType = number[]
-function findSmiler(arr1:NumberArrayType, arr2:NumberArrayType):NumberArrayType{
-    const newArray:NumberArrayType=[]
-    for(const num of arr1){
-        if (arr2.includes(num)){
-            newArray.push(num)
-        }
+// ===================== Problem NO 2 ===================== //
+type NumberArrayType = number[];
+function findSmiler(
+  arr1: NumberArrayType,
+  arr2: NumberArrayType
+): NumberArrayType {
+  const newArray: NumberArrayType = [];
+  for (const num of arr1) {
+    if (arr2.includes(num)) {
+      newArray.push(num);
     }
-    return newArray;
+  }
+  return newArray;
 }
-const fArray:NumberArrayType = [1, 2, 3, 4, 5];
-const sArray:NumberArrayType = [2, 4, 6, 8];
-console.log(findSmiler(fArray, sArray))
+const fArray: NumberArrayType = [1, 2, 3, 4, 5];
+const sArray: NumberArrayType = [2, 4, 6, 8];
+// console.log(findSmiler(fArray, sArray));
+
+// ===================== Problem NO 3 ===================== //
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+}
+
+const productArray = [
+  { id: 1, name: "Product A", price: 10, category: "Category 1" },
+  { id: 2, name: "Product B", price: 20, category: "Category 2" },
+  { id: 3, name: "Product C", price: 30, category: "Category 1" },
+];
+
+const filterWithCriterion = <T extends keyof Product>(
+  productArr: Product[],
+  key: T,
+  value: Product[T]
+): Product[] => {
+  return productArr.filter((item) => {
+    return item[key] === value;
+  });
+};
+
+const filterProduct = filterWithCriterion(
+  productArray,
+  "category",
+  "Category 1"
+);
+console.log(filterProduct);
+
+// ===================== Problem NO 4 ===================== //
+// ===================== Problem NO 5 ===================== //
+// ===================== Problem NO 6 ===================== //
+// ===================== Problem NO 7 ===================== //
+// ===================== Problem NO 8 ===================== //
