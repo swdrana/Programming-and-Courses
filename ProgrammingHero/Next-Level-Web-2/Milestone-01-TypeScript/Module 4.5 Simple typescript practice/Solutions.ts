50f31c2c8a205c25ee1e2f9d0ec58f30e355b07d
swdrana <tctr2s@gmail.com>
2023-06-23 11:31:00 +0600

    practicing 4.5 TS

---
+++ ProgrammingHero/Next-Level-Web-2/Milestone-01-TypeScript/Module 4.5 Simple typescript practice/Solutions.ts
@@ -0,0 +1,12 @@
+// Problem NO 1
+const userInfo: [number, string, string, boolean, undefined, string] = [
+  101,
+  "Ema",
+  "John",
+  true,
+  ,
+  "2023",
+];
+console.log(userInfo)
+
+// Problem NO 2
