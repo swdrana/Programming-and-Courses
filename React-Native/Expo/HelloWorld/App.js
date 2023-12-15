ea4923eccfe5be5a06e3a5b740b0333933e2ed20
swdrana <tctr2s@gmail.com>
2023-12-15 23:01:44 +0600

    pay with numberOfLines and onPress with useState

--- React-Native/Expo/HelloWorld/App.js
+++ React-Native/Expo/HelloWorld/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Rana</Text>
+      <Text numberOfLines={1} onPress={()=>setCount(count+1)}> {count} Rana Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam distinctio ducimus, nulla placeat alias, delectus maiores dolor vitae eligendi labore sint rerum facere quo doloribus illo quam ut! Illum iusto necessitatibus esse commodi, debitis velit expedita asperiores quibusdam perferendis, sequi autem soluta sed nulla, facilis aut! Nisi fugiat, at suscipit velit sint, distinctio dignissimos optio, accusantium laboriosam iste maxime nihil. Fugit delectus perferendis dolor est, sed ullam ad quia consequuntur quo eum culpa. Aliquid sint ab sit accusamus ea, animi temporibus ullam numquam cum consequatur dicta nostrum provident tempore eius expedita at blanditiis. Et aliquid ut cupiditate neque quibusdam libero!</Text>
       <StatusBar style="auto" />
     </View>
   );
