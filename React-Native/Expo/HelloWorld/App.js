5ccb5418256bce8fb36326c1781b2c0f42592545
swdrana <tctr2s@gmail.com>
2023-12-15 23:56:12 +0600

    play with alert and btn

--- React-Native/Expo/HelloWorld/App.js
+++ React-Native/Expo/HelloWorld/App.js
@@ -1,13 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 export default function App() {
   const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} onPress={()=>setCount(count+1)}> {count} Rana Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam distinctio ducimus, nulla placeat alias, delectus maiores dolor vitae eligendi labore sint rerum facere quo doloribus illo quam ut! Illum iusto necessitatibus esse commodi, debitis velit expedita asperiores quibusdam perferendis, sequi autem soluta sed nulla, facilis aut! Nisi fugiat, at suscipit velit sint, distinctio dignissimos optio, accusantium laboriosam iste maxime nihil. Fugit delectus perferendis dolor est, sed ullam ad quia consequuntur quo eum culpa. Aliquid sint ab sit accusamus ea, animi temporibus ullam numquam cum consequatur dicta nostrum provident tempore eius expedita at blanditiis. Et aliquid ut cupiditate neque quibusdam libero!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={() => setCount(count + 1)}>
+        My Count: {count} Rana Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quisquam distinctio ducimus, nulla placeat alias,
+        delectus maiores dolor vitae eligendi labore sint rerum facere quo
+        doloribus illo quam ut!
+      </Text>
+      {/* <Image source={require("./assets/icon.png")}/> */}
+      <TouchableWithoutFeedback onPress={() => setCount(count - 1)}>
+        <Image
+          source={{
+            width: 100,
+            height: 100,
+            uri: "https://freeiconshop.com/wp-content/uploads/edd/png-flat.png",
+          }}
+        />
+      </TouchableWithoutFeedback>
+      <Button title="alert()" onPress={() => alert("Button Tapped")} />
+      <Button
+        title="Alert.alert()"
+        color={"orange"}
+        onPress={() => Alert.alert("My Title", "Alert Body", [
+          {text:"YES", onPress: ()=>alert('Yes Clicked')},
+          {text:"No"},
+          {text:"Cancel"},
+          {text:"Not Show"}
+        ])}
+      />
+      {/* bellow btn not working in Android only for iOS  */}
+      <Button
+        title="Alert.promt() only for iOS"
+        color={"green"}
+        onPress={() => Alert.prompt("My Title", "Alert Body", (text)=>console.log(text))}
+      />
     </View>
   );
 }
@@ -15,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignContent: "center",
+    justifyContent: "center",
   },
 });
