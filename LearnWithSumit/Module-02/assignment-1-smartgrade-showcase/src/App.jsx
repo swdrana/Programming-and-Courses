2e7cc59b17ea1422c7a7883853cdacca43855283
swdrana <tctr2s@gmail.com>
2024-01-15 17:01:34 +0600

    assignment 1 done'

---
+++ LearnWithSumit/Module-02/assignment-1-smartgrade-showcase/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Students from "./components/Students";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Students />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ LearnWithSumit/Module-02/assignment-1-smartgrade-showcase/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import logo from '../assets/lws-logo-en.svg'
+function Navbar() {
+  return (
+    <nav className="py-6">
+      <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <a href="/">
+          <img className="h-[40px]" src={logo} alt="Lws" />
+        </a>
+        <a className="px-5 py-2 bg-[#172227] text-white rounded-[44px]" href="#">
+          Get Admission
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
