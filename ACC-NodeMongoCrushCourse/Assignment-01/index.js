8f0c25c75d744db8f63afdac7582a37bf2ddd8ef
swdrana <tctr2s@gmail.com>
2024-02-04 12:23:47 +0600

    server file index.js server function  added

--- ACC-NodeMongoCrushCourse/Assignment-01/index.js
+++ ACC-NodeMongoCrushCourse/Assignment-01/index.js
@@ -8,6 +8,20 @@ app.use('/user',userRoute)
 app.get('/',(req,res)=>{
     res.send(fs.readFileSync('./data/userData.json'))
 })
-app.listen(8080,()=>{
-    console.log('server is running')
-})
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
+  
+  const server = app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+  });
+  
+  process.on('SIGTERM', () => {
+    console.log('Received SIGTERM. Closing server...');
+    server.close(() => {
+      console.log('Server closed. Exiting process.');
+      process.exit(0);
+    });
+  });
+  
