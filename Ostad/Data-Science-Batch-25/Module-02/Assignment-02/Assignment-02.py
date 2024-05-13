ceee817ac885b529225655a83ae250080ecb4ce1
swdrana <tctr2s@gmail.com>
2024-05-13 23:56:58 +0600

    remove all files whice are not for level 2

--- Ostad/Data-Science-Batch-25/Module-02/Assignment-02/Assignment-02.py
+++
@@ -1,6 +0,0 @@
-def ascendingList(getList):
-    getList.sort(key=None, reverse=False)
-    return getList
-
-List = [1, 5, 2, 9, 3, 22, 13]
-print(ascendingList(List))
--- পাইথন দিয়ে প্রোগ্রামিং শেখা ১ম খণ্ড /Ch-03/07. Type & Sum.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b = 2.5
-c = a+b
-print(c) #Result automatically came: Float Type
-
-a = 1
-b = "2.5"
-#c = a+b
-print(c) #TypeError: unsupported operand type(s) for +: 'int' and 'str'
