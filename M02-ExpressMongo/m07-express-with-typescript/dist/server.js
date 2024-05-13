30a545ad01107e3f788d6a963f8e3a0ef61ef6fd
swdrana <tctr2s@gmail.com>
2024-05-14 00:26:08 +0600

    successfuly create express server using ts

--- M02-ExpressMongo/m07-express-with-typescript/dist/server.js
+++ M02-ExpressMongo/m07-express-with-typescript/dist/server.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hello');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.json({ success: true, data: "Get From Server" });
+});
+app.listen(8000, () => {
+    console.log('Server Connected');
+});
--- M02-ExpressMongo/m07-express-with-typescript/src/server.ts
+++ M02-ExpressMongo/m07-express-with-typescript/src/server.ts
@@ -1,1 +1,10 @@
-console.log('hello')
+import express from 'express';
+const app = express()
+
+app.get('/',(req:Request, res:Response)=>{
+    res.json({success:true, data:"Get From Server"})
+})
+
+app.listen(8000,()=>{
+    console.log('Server Connected')
+})
