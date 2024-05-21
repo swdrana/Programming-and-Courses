85dec212642c273a5bf44af165fb5847652ca8b8
swdrana <tctr2s@gmail.com>
2024-05-21 23:08:40 +0600

    ES Lint setup done

--- M02-ExpressMongo/m10-assignment-02-e-commerce-product/src/server.ts
+++ M02-ExpressMongo/m10-assignment-02-e-commerce-product/src/server.ts
@@ -5,6 +5,7 @@ import app from "./app";
 (async () => {
   const { port, mongoUrl } = env as { port: number; mongoUrl: string };
   try {
+    var a = 1;
     // server setup
     app.listen(port, async () => {
       console.log(`a2 e-Commerce backend app listening on port: ${port}`);
