37449537cf42f5493fc707f37b19c933a9899f15
swdrana <tctr2s@gmail.com>
2024-06-13 23:57:55 +0600

    working on users module

--- job-tasks/ticket-booking-dashboard/ticket-booking-dashboard-server/src/app.ts
+++ job-tasks/ticket-booking-dashboard/ticket-booking-dashboard-server/src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import userRouter from './modules/users/user.route';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
+
+app.use('/users', userRouter);
+
 
 export default app;
---
+++ job-tasks/ticket-booking-dashboard/ticket-booking-dashboard-server/src/modules/users/user.controller.ts
@@ -0,0 +1,5 @@
+const getAllUsers = (req: Request, res: Response) => {
+  res.send("Hello World!");
+};
+
+export const userController = { getAllUsers };
