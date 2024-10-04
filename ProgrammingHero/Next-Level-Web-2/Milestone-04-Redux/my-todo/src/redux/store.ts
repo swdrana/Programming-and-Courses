47e423fc54e5baa8673a0c3c28fc140ee07755f9
swdrana <tctr2s@gmail.com>
2024-10-04 22:55:18 +0800

    todo reducer connect with store

--- ProgrammingHero/Next-Level-Web-2/Milestone-04-Redux/my-todo/src/redux/store.ts
+++ ProgrammingHero/Next-Level-Web-2/Milestone-04-Redux/my-todo/src/redux/store.ts
@@ -1,4 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./features/todoSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    todos: todoReducer,
+  },
 });
+
+// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+export type AppStore = typeof store
