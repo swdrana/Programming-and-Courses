5fe2c202836ec470518fcef5595a57943c96fbcd
swdrana <tctr2s@gmail.com>
2024-10-04 21:57:52 +0800

    working on redux shadcn installed but not working

---
+++ ProgrammingHero/Next-Level-Web-2/Milestone-04-Redux/Module-25-Hands-on with Redux Toolkit/todo-module-3/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">My TO-DO</h1>
+      <Button> My Button</Button>
+      <Button variant="outline">Button</Button>
+    </>
+  );
+}
+
+export default App;
