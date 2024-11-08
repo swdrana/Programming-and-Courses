53a349054971b267434649cf424fdfc92cf1f130
swdrana <tctr2s@gmail.com>
2024-11-08 19:20:06 +0800

    Tuple rename and Sets start

---
+++ SUST/Python/NM_Class/Class-06-Sets.py
@@ -0,0 +1,8 @@
+# Sets: {}
+# Set not allow duplicates elements
+# Don't support indexing
+# Set is unordered
+
+# if duplicate elements then it will be removed
+s = {1,2,3,4,5,1,2,3,4,5,6}
+print(s) # {1, 2, 3, 4, 5, 6}
